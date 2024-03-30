c9f384a54d9729d7a4c94bd377f7c27ce3c37103
Natanael Neto <nesn.gamedev@gmail.com>
2024-03-29 21:17:22 -0300

    C4 - adiciona navegacao dos cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CardsComponent } from './pages/cards/cards.component';
 import { DecksComponent } from './pages/decks/decks.component';
+import { ListComponent } from './pages/cards/list/list.component';
+import { DetailsComponent } from './pages/cards/details/details.component';
+import { EditComponent } from './pages/cards/edit/edit.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home', title: 'PDM | Home' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent, title: 'PDM | Home' },
-  { path: 'cartas', component: CardsComponent, title: 'PDM | Cartas' },
+  {
+    path: 'cartas', component: CardsComponent, title: 'PDM | Cartas', children: [
+      { path: '', pathMatch: 'full', redirectTo: 'listar' },
+      { path: 'listar', component: ListComponent, title: 'PDM | Cartas - Lista' },
+      { path: 'detalhes', component: DetailsComponent, title: 'PDM | Cartas - Detalhes' },
+      { path: 'editar', component: EditComponent, title: 'PDM | Cartas - Editar' }
+    ]
+  },
   { path: 'decks', component: DecksComponent, title: 'PDM | Decks' },
 ];
 
