d1c7a77658cee27257dc820dcdd82f17a5c989fb
Natanael Neto <nesn.gamedev@gmail.com>
2024-03-31 00:01:04 -0300

    C5 - adiciona headers da lista

--- src/app/pages/cards/list/list.component.ts
+++ src/app/pages/cards/list/list.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  typeFilter = [
+    {
+      type: 'Incolor',
+      filtered: false,
+    },
+    {
+      type: 'Planta',
+      filtered: false,
+    },
+    {
+      type: 'Fogo',
+      filtered: false,
+    },
+    {
+      type: 'Água',
+      filtered: false,
+    },
+    {
+      type: 'Luta',
+      filtered: false,
+    },
+    {
+      type: 'Raio',
+      filtered: false,
+    },
+    {
+      type: 'Psíquica',
+      filtered: false,
+    },
+    {
+      type: 'Metal',
+      filtered: false,
+    },
+    {
+      type: 'Escuridão',
+      filtered: false,
+    },
+  ]
+  allTypeFiltered = false;
+  typeFilterDisplay = false;
 
+  constructor() { }
+
+  toggleTypeFilter(type: string) {
+    if (type == 'Todos') {
+      this.allTypeFiltered = !this.allTypeFiltered;
+      this.typeFilter.forEach(t => t.filtered = this.allTypeFiltered);
+      return;
+    }
+    this.typeFilter.forEach(t => {
+      if (t.type == type) t.filtered = !t.filtered;
+    })
+    this.allTypeFiltered = this.typeFilter.every(t => t.filtered);
+  }
 }
